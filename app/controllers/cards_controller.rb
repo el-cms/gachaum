567751824be42f31e1dab846b514d35995a46ab3
Romuald Conty <neomilium@gmail.com>
2018-06-05 19:47:56 +0200

    Add card (printed) number

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -70,6 +70,6 @@ class CardsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def card_params
-    params.require(:card).permit(:uid, :user_id)
+    params.require(:card).permit(:uid, :number, :user_id)
   end
 end
